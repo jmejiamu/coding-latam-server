2043b2471af549936ad077adee2c0b3d743e9dee
Jose <jmejiamu@gmail.com>
2021-03-01 21:12:57 -0800

    setup server

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const knex = require('knex');
+const cors = require('cors');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/test', (req, res) => {
+    res.status(200).send("This is a test")
+})
+
+
+app.listen(3006, process.env.URL, () => {
+    console.log("The app is running at port 3006");
+})
