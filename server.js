0f11fbf7a1980aade872c9c74e0c4844b32513cc
Jose <jmejiamu@gmail.com>
2021-03-01 21:32:33 -0800

    added DB connection

--- server.js
+++ server.js
@@ -19,10 +19,16 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// Import Routes
+const getUser = require('./routes/get');
+
 // Test get method route
 app.get('/test', (req, res) => {
     res.status(200).send("This is a test")
 })
+
+// Get all user info
+
 
 
 app.listen(3006, process.env.URL, () => {
